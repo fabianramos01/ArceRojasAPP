312798b036427ce3235ac738a5c1f79b9be549b2
Fabián Ramos <fabian.ramos01@uptc.edu.co>
2022-02-11 17:42:00 -0500

    Add configuration for static files

--- test_app/settings.py
+++ test_app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from os.path import join
 from pathlib import Path
 
 from decouple import config
@@ -125,8 +125,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_ROOT = join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
